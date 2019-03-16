ac096ebf74eff5df155aa90114bae808707b6870
James Walker <jh_walker@outlook.com>
2019-03-16 12:12:24 +0000

    MSFTbandLibUWP implement BandClient

--- msftbandlib-uwp/src/MSFTBandLibUWP.cs
+++ msftbandlib-uwp/src/MSFTBandLibUWP.cs
@@ -1,17 +1,38 @@
 using MSFTBandLib;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Rfcomm;
 
 namespace MSFTBandLibUWP {
 
 /// <summary>
 /// MSFTBandLib UWP implementation
 /// </summary>
-public static class MSFTBandLibUWP {
+public class MSFTBandLibUWP : BandClient<BandSocketUWP> {
 
-	/// <summary>Create a new Band connection.</summary>
-	/// <param name="band">Band instance</param>
-	/// <returns>BandConnection</returns>
-	public static BandConnection<BandSocketUWP> Connection(Band band) {
-		return new BandConnection<BandSocketUWP>(band);
+	public BandConnection<BandSocket> GetConnection(Band band) {
+		BandConnection<BandSocketUWP> connection;
+		connection = new BandConnection<BandSocketUWP>(band);
+		return (BandConnection<BandSocket>) (object) connection;
+	}
+
+	public async Task<List<Band>> GetPairedBands() {
+		string selector;
+		RfcommServiceId cargo;
+		DeviceInformationCollection devices;
+		List<Band> bands = new List<Band>();
+		cargo = RfcommServiceId.FromUuid(Band.CARGO);
+		selector = RfcommDeviceService.GetDeviceSelector(cargo);
+		devices = await DeviceInformation.FindAllAsync(selector);
+		foreach (DeviceInformation device in devices) {
+			BluetoothDevice bt;
+			bt = await BluetoothDevice.FromIdAsync(device.Id);
+			bands.Add(new Band(bt.HostName.ToString(), bt.Name));
+		}
+		return bands;
 	}
 
 }
